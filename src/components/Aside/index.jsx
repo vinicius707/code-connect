ddf2d5f44b0e8426e87bd3f064912cd541d9ab9c
Vinicius <viniciussilva707@gmail.com>
2025-10-17 20:13:52 -0300

    fix - extensão do arquivo

---
+++ src/components/Aside/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./aside.module.css";
+import logo from './logo.png';
+import Image from 'next/image';
+
+const Aside = () => {
+    return (<aside className={styles.aside}>
+        <Image src={logo} alt="Logo do Code Connect" />
+    </aside>)
+}
+
+export default Aside;
